bc53b1eacd653fb5eb4aa068da16a493f21d32e0
Abel Puentes Luberta <apuentes@mychoice2pay.com>
2017-09-18 16:47:40 +0200

    Added resources file

---
+++ lib/resources.rb
@@ -0,0 +1,116 @@
+module MC2P
+
+  # Product resource
+  class ProductResource < CRUDResource
+
+  end
+
+  # Plan resource
+  class PlanResource < CRUDResource
+
+  end
+
+  # Tax resource
+  class TaxResource < CRUDResource
+
+  end
+
+  # Shipping resource
+  class ShippingResource < CRUDResource
+
+  end
+
+  # Coupon resource
+  class CouponResource < CRUDResource
+
+  end
+
+  # Transaction resource
+  class TransactionResource < CRResource
+
+  end
+
+  # Subscription resource
+  class SubscriptionResource < CRResource
+
+  end
+
+  # Sale resource
+  class SaleResource < ReadOnlyResource
+    # Initializes a resource
+    # Params:
+    # +api_request+:: Api request used to make all the requests to the API
+    # +path+:: Path used to make all the requests to the API
+    # +object_item_class+:: Object item class used to return values
+    def initialize(api_request, path, object_item_class)
+      @rcv_resource_mixin = RefundCaptureVoidResourceMixin.new(api_request, path, object_item_class, @paginator_class)
+      super(api_request, path, object_item_class)
+    end
+
+    # Params:
+    # +resource_id+:: id to request
+    # +data+:: data to send
+    # Returns: response dictionary
+    def refund(resource_id, data = nil)
+      @rcv_resource_mixin.refund(resource_id, data)
+    end
+
+    # Params:
+    # +resource_id+:: id to request
+    # +data+:: data to send
+    # Returns: response dictionary
+    def capture(resource_id, data = nil)
+      @rcv_resource_mixin.capture(resource_id, data)
+    end
+
+    # Params:
+    # +resource_id+:: id to request
+    # +data+:: data to send
+    # Returns: response dictionary
+    def void(resource_id, data = nil)
+      @rcv_resource_mixin.void(resource_id, data)
+    end
+  end
+
+  # Currency resource
+  class CurrencyResource < ReadOnlyResource
+
+  end
+
+  # Gateway resource
+  class GatewayResource < ReadOnlyResource
+
+  end
+
+  # PayData resource
+  class PayDataResource < DetailOnlyResource
+    # Initializes a resource
+    # Params:
+    # +api_request+:: Api request used to make all the requests to the API
+    # +path+:: Path used to make all the requests to the API
+    # +object_item_class+:: Object item class used to return values
+    def initialize(api_request, path, object_item_class)
+      @cs_resource_mixin = CardShareResourceMixin.new(api_request, path, object_item_class, @paginator_class)
+      super(api_request, path, object_item_class)
+    end
+
+    # Params:
+    # +resource_id+:: id to request
+    # +gateway_code+:: gateway_code to send
+    # +data+:: data to send
+    # Returns: response dictionary
+    def card(resource_id, gateway_code, data = nil)
+      @cs_resource_mixin.card(resource_id, gateway_code, data)
+    end
+
+    # Params:
+    # +resource_id+:: id to request
+    # +data+:: data to send
+    # Returns: response dictionary
+    def share(resource_id, data = nil)
+      @cs_resource_mixin.share(resource_id, data)
+    end
+  end
+
+end
+
