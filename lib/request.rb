b2d6b7443675e841e8ee9fcf6cee153d619ed4f6
Abel Puentes Luberta <apuentes@mychoice2pay.com>
2017-09-18 15:28:46 +0200

    Fixed request file

--- lib/request.rb
+++ lib/request.rb
@@ -1,13 +1,9 @@
-require "unirest"
-require "errors"
+module MC2P
 
-
-class APIRequest
-    """
-    API request - class used to connect with the API
-    """
-    AUTHORIZATION_HEADER = "AppKeys"
-    API_URL = "api.mychoice2pay.com/v1"
+  # API request - class used to connect with the API
+  class APIRequest
+    @authorization_header = 'AppKeys'
+    @api_url = 'api.mychoice2pay.com/v1'
 
     attr_accessor :post
     attr_accessor :post_200
@@ -15,71 +11,85 @@ class APIRequest
     attr_accessor :patch
     attr_accessor :delete
 
-    def initialize(key, secret_key):
-        """
-        Initializes an api request
-        :param key: key to connect with API
-        :param secret_key: secret key to connect with API
-        """
-        @key = key
-        @secret_key = secret_key
-
-        @post = self._request('POST', 201)
-        @post_200 = self._request('POST', 200)
-        @get = self._request('GET')
-        @patch = self._request('PATCH')
-        @delete = self._request('DELETE', 204)
+    # Initializes an api request
+    # Params:
+    # +key+:: key to connect with API
+    # +secret_key+:: secret key to connect with API
+    def initialize(key, secret_key)
+      @key = key
+      @secret_key = secret_key
     end
 
+    # Creates the headers to include in the request
+    # Returns: A dictionary with the headers needed for the API
     def headers
-        """
-        Creates the headers to include in the request
-        :return: A dictionary with the headers needed for the API
-        """
-        {
-            "authorization" => "#{@AUTHORIZATION_HEADER} #{@key}:#{@secret_key}",
-            "content-type": "application/json"
-        }
+      {
+        'authorization' => "#{@authorization_header} #{@key}:#{@secret_key}",
+        'content-type' => 'application/json'
+      }
     end
 
-    def get_abs_url(self, path):
-        """
-        :param path: relative url
-        :return: The absolute url to send the request
-        """
-        "https://#{@API_URL}#{path}"
+    # Params:
+    # +path+:: relative url
+    # Returns: The absolute url to send the request
+    def get_abs_url(path)
+      "https://#{@api_url}#{path}"
     end
 
-    def _request(self, method, status_code=200):
-        """
-        Decorator to make the request based on the method received
-        :param method: method to make the request
-        :param status_code: value to check if the request receive a correct response
-        :return: a function to make the request
-        """
-        def func(path=nil, data=nil, abs_url=nil, resource=nil, resource_id=nil):
-            if abs_url
-                url = abs_url
-            else
-                url = self.get_abs_url(path)
+    # Decorator to make the request based on the method received
+    # Params:
+    # +method+:: method to make the request
+    # +status_code+:: value to check if the request receive a correct response
+    # Returns: a function to make the request
+    def _request(method, status_code=200, path=nil, data=nil,
+                 abs_url=nil, resource=nil, resource_id=nil)
+      url = abs_url.nil? ? get_abs_url(path) : abs_url
 
-            request = Unirest.send(method.downcase)(url,
-                headers:self.headers,
-                parameters:data)
+      request = Unirest.send(method.downcase, url,
+                             headers: headers,
+                             parameters: data)
 
-            if request.code != status_code
-                raise InvalidRequestError.new(
-                    'Error %s' % request.code,
-                    json_body:request.to_json,
-                    resource:resource,
-                    resource_id:resource_id
-                )
+      if request.code != status_code
+        raise InvalidRequestError.new(
+          "Error #{request.code}",
+          json_body: request.to_json,
+          resource: resource,
+          resource_id: resource_id
+        )
+      end
 
-            ret = {}
-            if status_code != 204
-                ret = request.to_json
-            ret
-        end
-        func
+      status_code != 204 ? request.to_json : {}
     end
+
+    # POST 201 function
+    def post(path = nil, data = nil, abs_url = nil,
+             resource = nil, resource_id = nil)
+      _request('post', 201, path, data, abs_url, resource, resource_id)
+    end
+
+    # POST 200 function
+    def post200(path = nil, data = nil, abs_url = nil,
+                resource = nil, resource_id = nil)
+      _request('post', 200, path, data, abs_url, resource, resource_id)
+    end
+
+    # GET 200 function
+    def get(path = nil, data = nil, abs_url = nil,
+            resource = nil, resource_id = nil)
+      _request('get', 200, path, data, abs_url, resource, resource_id)
+    end
+
+    # PATCH 200 function
+    def patch(path = nil, data = nil, abs_url = nil,
+              resource = nil, resource_id = nil)
+      _request('patch', 200, path, data, abs_url, resource, resource_id)
+    end
+
+    # DELETE 204 function
+    def delete(path = nil, data = nil, abs_url = nil,
+               resource = nil, resource_id = nil)
+      _request('delete', 204, path, data, abs_url, resource, resource_id)
+    end
+  end
+
 end
