7e8b9cb84910e90d7a7a5fc59b5be9814594a187
Abel Puentes Luberta <apuentes@mychoice2pay.com>
2017-09-18 14:55:58 +0200

    Fixed errors file

--- lib/errors.rb
+++ lib/errors.rb
@@ -1,45 +1,42 @@
-class MC2PError < Exception
-    """
-    MC2P Error - class used to manage the exceptions related with mc2p library
-    """
+module MC2P
+
+  # MC2P Error - class used to manage the exceptions related with mc2p library
+  class MC2PError < StandardError
     attr_accessor :json_body
     attr_accessor :resource
     attr_accessor :resource_id
 
-    def initialize(message=None, json_body=None, resource=None, resource_id=None):
-        """
-        Initializes an error
-        :param message: Error type
-        :param json_body: Response from server
-        :param resource: Class resource used when the error raised
-        :param resource_id: Resource id requested when the error raised
-        """
-        super(message)
+    # Initializes an error
+    # Params:
+    # +message+:: Error type
+    # +json_body+:: Response from server
+    # +resource+:: Class resource used when the error raised
+    # +resource_id+:: Resource id requested when the error raised
+    def initialize(message=nil, json_body=nil, resource=nil, resource_id=nil)
+      super(message)
 
-        @_message = message
-        @json_body = json_body
-        @resource = resource
-        @resource_id = resource_id
+      @_message = message
+      @json_body = json_body
+      @resource = resource
+      @resource_id = resource_id
     end
 
+    # Params:
+    # +return+:: Error type and response
     def to_s
-        """
-        :return: Error type and response
-        """
-        return "#{@_message} #{@json_body}"
+      "#{@_message} #{@json_body}"
     end
-end
+  end
+
+  # Invalid request error
+  class InvalidRequestError < MC2PError
+
+  end
 
 
-class InvalidRequestError < MC2PError
-    """
-    Invalid request error
-    """
-end
+  # Bad use error
+  class BadUseError < MC2PError
 
+  end
 
-class BadUseError < MC2PError
-    """
-    Bad use error
-    """
 end
